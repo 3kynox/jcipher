812dc70832885492db50c2d5a3a8d7a70130a185
3KyNoX <darkarmada1@yahoo.com>
2017-02-09 21:29:32 +0100

    Format a text and return it without spaces, specials and accents

--- JeffersonShell.py
+++ JeffersonShell.py
@@ -1,6 +1,20 @@
+import collections
+import string
+
 # Take a text and format it without spaces, accents and special chars
 def convertLetters(text):
-    return text
+    table = collections.defaultdict(lambda: None)
+    table.update({
+        ord('é'):'e', ord('ë'):'e', ord('é'):'e', ord('è'):'e',
+        ord('ô'):'o', ord('ö'):'o', ord('ò'):'o', ord(' '):'',
+        ord('â'):'a', ord('ä'):'a', ord('à'):'a', ord('à'):'a',
+        ord('û'):'u', ord('ü'):'u', ord('ù'):'u', ord('ç'):'c',
+        })
+    table.update(dict(zip(map(ord,string.ascii_uppercase), string.ascii_lowercase)))
+    table.update(dict(zip(map(ord,string.ascii_lowercase), string.ascii_lowercase)))
+    table.update(dict(zip(map(ord,string.digits), string.digits)))
+    
+    return text.translate(table,)
 
 userInput = input('Enter your input: ')
 print(convertLetters(userInput))
