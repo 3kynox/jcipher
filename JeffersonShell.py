c52b4cd9331e101662bb770107bc65ee88bad445
3KyNoX <darkarmada1@yahoo.com>
2017-02-09 21:04:31 +0100

    Simple userInput and function call

---
+++ JeffersonShell.py
@@ -0,0 +1,7 @@
+# Take a text and format it without spaces, accents and special chars
+def convertLetters(text):
+    return text
+
+userInput = input('Enter your input: ')
+print(convertLetters(userInput))
+
