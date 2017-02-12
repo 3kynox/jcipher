70afa571a67b1361c62a80e3e0d5e7604770473a
3KyNoX <darkarmada1@yahoo.com>
2017-02-12 14:08:05 +0100

    enterKey() function - Hard ...

--- JeffersonGUI.py
+++ JeffersonGUI.py
@@ -7,7 +7,7 @@ pygame.init()
 
 def main():
     # Init vars
-    global n
+    global n, selectedItems, keyList
     play_again = 1
     size = width, height = 800, 750
     screen = pygame.display.set_mode(size)
@@ -15,6 +15,8 @@ def main():
     surface = surface.convert()
     surface.fill((0, 0, 0))
     font = pygame.font.Font('AgencyFB_Light_Wide.ttf', 20)
+    selectedItems = {}
+    keyList = []
 
     # Define later ability to load cylinder file
     cylinder = loadCylinder('cylinder.txt')
@@ -48,18 +50,46 @@ def main():
             y = y + 40
 
     def enterKey(mySurface, n):
-        x = 25 + (26 * 25)
+        global wheelKeys
+        x = 50 + (26 * 25)
         y = 10
+        texts = []
 
         for key, value in cylinder.items():
             text = str(key)
             renderText = font.render(text, 0, (249, 0, 0))
-            mySurface.blit(renderText, (y, x))
+            texts.append(renderText)
+            renderSurface = mySurface.blit(renderText, (y, x))
+
+            if event.type == MOUSEBUTTONDOWN and renderSurface.collidepoint(pygame.mouse.get_pos()):
+                selectedItems[key] = value
+
+                if key not in keyList:
+                    keyList.append(key)
+
+            if key in selectedItems:
+                texts[key - 1] = font.render(text, 0, (255, 255, 255))
+                
+            renderSurface = mySurface.blit(texts[key - 1], (y, x))
+
             y = y + 40
+
+        
+        v = 10
+        w = 80 + (26 * 25)
+
+        for a in keyList:
+            keyText = str(a)
+            renderKey = font.render(keyText, 0, (255, 255, 255))
+            mySurface.blit(renderKey, (v, w))
+            v = v + 40
 
         text = 'ENTER THE KEY'
         renderText = font.render(text, 0, (249, 0, 0))
         mySurface.blit(renderText, (y + 25 , x))
+
+        return keyList
+                
 
     def rotateCylinder(cylinder, i, up = True):
         return True
@@ -70,7 +100,6 @@ def main():
     # Main Loop
     while play_again:
         displayCylinders(surface, cylinder)
-        enterKey(surface, n)
         
         # Events loop
         for event in pygame.event.get():
@@ -78,6 +107,8 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            myKeyList = enterKey(surface, n)
         
         screen.blit(surface, (0, 0))
         pygame.display.flip()
